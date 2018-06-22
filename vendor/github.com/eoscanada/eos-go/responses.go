fec6eca43bd402b1d5395957f8069e8b7e355bae
Alexandre Bourget <alex@bourget.cc>
2018-06-22 01:28:46 -0400

    Vendored deps.

---
+++ vendor/github.com/eoscanada/eos-go/responses.go
@@ -0,0 +1,304 @@
+package eos
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"reflect"
+
+	"github.com/eoscanada/eos-go/ecc"
+)
+
+/*
+{
+  "server_version": "f537bc50",
+  "head_block_num": 9,
+  "last_irreversible_block_num": 8,
+  "last_irreversible_block_id": "00000008f98f0580d7efe7abc60abaaf8a865c9428a4267df30ff7d1937a1084",
+  "head_block_id": "00000009ecd0e9fb5719431f4b86f5c9ca1887f6b6f73e5a301aaff740fd6bd3",
+  "head_block_time": "2018-05-19T07:47:31",
+  "head_block_producer": "eosio",
+  "virtual_block_cpu_limit": 100800,
+  "virtual_block_net_limit": 1056996,
+  "block_cpu_limit": 99900,
+  "block_net_limit": 1048576
+}
+
+*/
+
+type InfoResp struct {
+	ServerVersion            string      `json:"server_version"` // "2cc40a4e"
+	ChainID                  SHA256Bytes `json:"chain_id"`
+	HeadBlockNum             uint32      `json:"head_block_num"`              // 2465669,
+	LastIrreversibleBlockNum uint32      `json:"last_irreversible_block_num"` // 2465655
+	LastIrreversibleBlockID  SHA256Bytes `json:"last_irreversible_block_id"`  // "00000008f98f0580d7efe7abc60abaaf8a865c9428a4267df30ff7d1937a1084"
+	HeadBlockID              SHA256Bytes `json:"head_block_id"`               // "00259f856bfa142d1d60aff77e70f0c4f3eab30789e9539d2684f9f8758f1b88",
+	HeadBlockTime            JSONTime    `json:"head_block_time"`             //  "2018-02-02T04:19:32"
+	HeadBlockProducer        AccountName `json:"head_block_producer"`         // "inita"
+
+	VirtualBlockCPULimit uint64 `json:"virtual_block_cpu_limit"`
+	VirtualBlockNetLimit uint64 `json:"virtual_block_net_limit"`
+	BlockCPULimit        uint64 `json:"block_cpu_limit"`
+	BlockNetLimit        uint64 `json:"block_net_limit"`
+}
+
+type BlockResp struct {
+	SignedBlock
+	ID              SHA256Bytes  `json:"id"`
+	BlockNum        uint32       `json:"block_num"`
+	RefBlockPrefix  uint32       `json:"ref_block_prefix"`
+	BlockExtensions []*Extension `json:"block_extensions"`
+}
+
+// type BlockTransaction struct {
+// 	Status        string            `json:"status"`
+// 	CPUUsageUS    int               `json:"cpu_usage_us"`
+// 	NetUsageWords int               `json:"net_usage_words"`
+// 	Trx           []json.RawMessage `json:"trx"`
+// }
+
+type TransactionResp struct {
+	ID      SHA256Bytes `json:"id"`
+	Receipt struct {
+		Status            TransactionStatus `json:"status"`
+		CPUUsageMicrosec  int               `json:"cpu_usage_us"`
+		NetUsageWords     int               `json:"net_usage_words"`
+		PackedTransaction TransactionWithID `json:"trx"`
+	} `json:"receipt"`
+	Transaction           ProcessedTransaction `json:"trx"`
+	BlockTime             JSONTime             `json:"block_time"`
+	BlockNum              uint32               `json:"block_num"`
+	LastIrreversibleBlock uint32               `json:"last_irreversible_block"`
+	Traces                []TransactionTrace   `json:"traces"`
+}
+
+type ProcessedTransaction struct {
+	Transaction SignedTransaction `json:"trx"`
+}
+
+type TransactionTrace struct {
+	Receipt struct {
+		Receiver        AccountName `json:"receiver"`
+		GlobalSequence  int         `json:"global_sequence"`
+		ReceiveSequence int         `json:"recv_sequence"`
+		// AuthSequence.. complex..
+		CodeSequence int `json:"code_sequence"`
+		ABISequence  int `json:"abi_sequence"`
+	} `json:"receipt"`
+	Action        Action      `json:"act"`
+	Elapsed       int         `json:"elapsed"`
+	CPUUsage      int         `json:"cpu_usage"`
+	Console       string      `json:"console"`
+	TotalCPUUsage int         `json:"total_cpu_usage"`
+	TransactionID SHA256Bytes `json:"trx_id"`
+	// InlineTraces ??
+}
+
+type SequencedTransactionResp struct {
+	SeqNum int `json:"seq_num"`
+	TransactionResp
+}
+
+type TransactionsResp struct {
+	Transactions []SequencedTransactionResp
+}
+
+type ProducerChange struct {
+}
+
+type AccountResp struct {
+	AccountName        AccountName          `json:"account_name"`
+	Privileged         bool                 `json:"privileged"`
+	LastCodeUpdate     JSONTime             `json:"last_code_update"`
+	Created            JSONTime             `json:"created"`
+	RAMQuota           int64                `json:"ram_quota"`
+	RAMUsage           int64                `json:"ram_usage"`
+	NetWeight          JSONInt64            `json:"net_weight"`
+	CPUWeight          JSONInt64            `json:"cpu_weight"`
+	NetLimit           AccountResourceLimit `json:"net_limit"`
+	CPULimit           AccountResourceLimit `json:"cpu_limit"`
+	Permissions        []Permission         `json:"permissions"`
+	TotalResources     TotalResources       `json:"total_resources"`
+	DelegatedBandwidth DelegatedBandwidth   `json:"delegated_bandwidth"`
+	VoterInfo          VoterInfo            `json:"voter_info"`
+}
+
+type CurrencyBalanceResp struct {
+	EOSBalance        Asset    `json:"eos_balance"`
+	StakedBalance     Asset    `json:"staked_balance"`
+	UnstakingBalance  Asset    `json:"unstaking_balance"`
+	LastUnstakingTime JSONTime `json:"last_unstaking_time"`
+}
+
+type GetTableRowsRequest struct {
+	JSON       bool   `json:"json"`
+	Scope      string `json:"scope"`
+	Code       string `json:"code"`
+	Table      string `json:"table"`
+	TableKey   string `json:"table_key"`
+	LowerBound string `json:"lower_bound"`
+	UpperBound string `json:"upper_bound"`
+	Limit      uint32 `json:"limit,omitempty"` // defaults to 10 => chain_plugin.hpp:struct get_table_rows_params
+}
+
+type GetTableRowsResp struct {
+	More bool            `json:"more"`
+	Rows json.RawMessage `json:"rows"` // defer loading, as it depends on `JSON` being true/false.
+}
+
+func (resp *GetTableRowsResp) JSONToStructs(v interface{}) error {
+	return json.Unmarshal(resp.Rows, v)
+}
+
+func (resp *GetTableRowsResp) BinaryToStructs(v interface{}) error {
+	var rows []string
+
+	err := json.Unmarshal(resp.Rows, &rows)
+	if err != nil {
+		return err
+	}
+
+	outSlice := reflect.ValueOf(v).Elem()
+	structType := reflect.TypeOf(v).Elem().Elem()
+
+	for _, row := range rows {
+		bin, err := hex.DecodeString(row)
+		if err != nil {
+			return err
+		}
+
+		// access the type of the `Slice`, create a bunch of them..
+		newStruct := reflect.New(structType)
+
+		decoder := NewDecoder(bin)
+		if err := decoder.Decode(newStruct.Interface()); err != nil {
+			return err
+		}
+
+		outSlice = reflect.Append(outSlice, reflect.Indirect(newStruct))
+	}
+
+	reflect.ValueOf(v).Elem().Set(outSlice)
+
+	return nil
+}
+
+type Currency struct {
+	Precision uint8
+	Name      CurrencyName
+}
+
+type GetRequiredKeysResp struct {
+	RequiredKeys []ecc.PublicKey `json:"required_keys"`
+}
+
+// PushTransactionFullResp unwraps the responses from a successful `push_transaction`.
+// FIXME: REVIEW the actual output, things have moved here.
+type PushTransactionFullResp struct {
+	StatusCode    string
+	TransactionID string               `json:"transaction_id"`
+	Processed     TransactionProcessed `json:"processed"` // WARN: is an `fc::variant` in server..
+}
+
+type TransactionProcessed struct {
+	Status               string        `json:"status"`
+	ID                   SHA256Bytes   `json:"id"`
+	ActionTraces         []ActionTrace `json:"action_traces"`
+	DeferredTransactions []string      `json:"deferred_transactions"` // that's not right... dig to find what's there..
+}
+
+type ActionTrace struct {
+	Receiver AccountName `json:"receiver"`
+	// Action     Action       `json:"act"` // FIXME: how do we unpack that ? what's on the other side anyway?
+	Console    string       `json:"console"`
+	DataAccess []DataAccess `json:"data_access"`
+}
+
+type DataAccess struct {
+	Type     string      `json:"type"` // "write", "read"?
+	Code     AccountName `json:"code"`
+	Scope    AccountName `json:"scope"`
+	Sequence int         `json:"sequence"`
+}
+
+type PushTransactionShortResp struct {
+	TransactionID string `json:"transaction_id"`
+	Processed     bool   `json:"processed"` // WARN: is an `fc::variant` in server..
+}
+
+//
+
+type WalletSignTransactionResp struct {
+	// Ignore the rest of the transaction, so the wallet server
+	// doesn't forge some transactions on your behalf, and you send it
+	// to the network..  ... although.. it's better if you can trust
+	// your wallet !
+
+	Signatures []ecc.Signature `json:"signatures"`
+}
+
+type MyStruct struct {
+	Currency
+	Balance uint64
+}
+
+// NetConnectionResp
+type NetConnectionsResp struct {
+	Peer          string           `json:"peer"`
+	Connecting    bool             `json:"connecting"`
+	Syncing       bool             `json:"syncing"`
+	LastHandshake HandshakeMessage `json:"last_handshake"`
+}
+
+type NetStatusResp struct {
+}
+
+type NetConnectResp string
+
+type NetDisconnectResp string
+
+type Global struct {
+	MaxBlockNetUsage               int       `json:"max_block_net_usage"`
+	TargetBlockNetUsagePct         int       `json:"target_block_net_usage_pct"`
+	MaxTransactionNetUsage         int       `json:"max_transaction_net_usage"`
+	BasePerTransactionNetUsage     int       `json:"base_per_transaction_net_usage"`
+	NetUsageLeeway                 int       `json:"net_usage_leeway"`
+	ContextFreeDiscountNetUsageNum int       `json:"context_free_discount_net_usage_num"`
+	ContextFreeDiscountNetUsageDen int       `json:"context_free_discount_net_usage_den"`
+	MaxBlockCPUUsage               int       `json:"max_block_cpu_usage"`
+	TargetBlockCPUUsagePct         int       `json:"target_block_cpu_usage_pct"`
+	MaxTransactionCPUUsage         int       `json:"max_transaction_cpu_usage"`
+	MinTransactionCPUUsage         int       `json:"min_transaction_cpu_usage"`
+	MaxTransactionLifetime         int       `json:"max_transaction_lifetime"`
+	DeferredTrxExpirationWindow    int       `json:"deferred_trx_expiration_window"`
+	MaxTransactionDelay            int       `json:"max_transaction_delay"`
+	MaxInlineActionSize            int       `json:"max_inline_action_size"`
+	MaxInlineActionDepth           int       `json:"max_inline_action_depth"`
+	MaxAuthorityDepth              int       `json:"max_authority_depth"`
+	MaxRAMSize                     string    `json:"max_ram_size"`
+	TotalRAMBytesReserved          JSONInt64 `json:"total_ram_bytes_reserved"`
+	TotalRAMStake                  JSONInt64 `json:"total_ram_stake"`
+	LastProducerScheduleUpdate     string    `json:"last_producer_schedule_update"`
+	LastPervoteBucketFill          int64     `json:"last_pervote_bucket_fill,string"`
+	PervoteBucket                  int       `json:"pervote_bucket"`
+	PerblockBucket                 int       `json:"perblock_bucket"`
+	TotalUnpaidBlocks              int       `json:"total_unpaid_blocks"`
+	TotalActivatedStake            float64   `json:"total_activated_stake,string"`
+	ThreshActivatedStakeTime       int64     `json:"thresh_activated_stake_time,string"`
+	LastProducerScheduleSize       int       `json:"last_producer_schedule_size"`
+	TotalProducerVoteWeight        float64   `json:"total_producer_vote_weight,string"`
+	LastNameClose                  string    `json:"last_name_close"`
+}
+
+type Producer struct {
+	Owner         string      `json:"owner"`
+	TotalVotes    float64     `json:"total_votes,string"`
+	ProducerKey   string      `json:"producer_key"`
+	IsActive      int         `json:"is_active"`
+	URL           string      `json:"url"`
+	UnpaidBlocks  int         `json:"unpaid_blocks"`
+	LastClaimTime JSONFloat64 `json:"last_claim_time"`
+	Location      int         `json:"location"`
+}
+type ProducersResp struct {
+	Producers []Producer `json:"producers"`
+}
---
+++ vendor/github.com/eoscanada/eos-go/types.go
@@ -0,0 +1,460 @@
+package eos
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/eoscanada/eos-go/ecc"
+)
+
+// For reference:
+// https://github.com/mithrilcoin-io/EosCommander/blob/master/app/src/main/java/io/mithrilcoin/eoscommander/data/remote/model/types/EosByteWriter.java
+
+type Name string
+type AccountName Name
+type PermissionName Name
+type ActionName Name
+type TableName Name
+type ScopeName Name
+
+func AN(in string) AccountName    { return AccountName(in) }
+func ActN(in string) ActionName   { return ActionName(in) }
+func PN(in string) PermissionName { return PermissionName(in) }
+
+type AccountResourceLimit struct {
+	Used      JSONInt64 `json:"used"`
+	Available JSONInt64 `json:"available"`
+	Max       JSONInt64 `json:"max"`
+}
+
+type DelegatedBandwidth struct {
+	From      AccountName `json:"from"`
+	To        AccountName `json:"to"`
+	NetWeight Asset       `json:"net_weight"`
+	CPUWeight Asset       `json:"cpu_weight"`
+}
+
+type TotalResources struct {
+	Owner     AccountName `json:"owner"`
+	NetWeight Asset       `json:"net_weight"`
+	CPUWeight Asset       `json:"cpu_weight"`
+	RAMBytes  JSONInt64   `json:"ram_bytes"`
+}
+
+type VoterInfo struct {
+	Owner             AccountName    `json:"owner"`
+	Proxy             AccountName    `json:"proxy"`
+	Producers         []AccountName  `json:"producers"`
+	Staked            JSONInt64      `json:"staked"`
+	LastVoteWeight    JSONFloat64    `json:"last_vote_weight"`
+	ProxiedVoteWeight JSONFloat64    `json:"proxied_vote_weight"`
+	IsProxy           byte           `json:"is_proxy"`
+	DeferredTrxID     uint32         `json:"deferred_trx_id"`
+	LastUnstakeTime   BlockTimestamp `json:"last_unstake_time"`
+	Unstaking         Asset          `json:"unstaking"`
+}
+
+type CompressionType uint8
+
+const (
+	CompressionNone = CompressionType(iota)
+	CompressionZlib
+)
+
+func (c CompressionType) String() string {
+	switch c {
+	case CompressionNone:
+		return "none"
+	case CompressionZlib:
+		return "zlib"
+	default:
+		return ""
+	}
+}
+
+func (c CompressionType) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.String())
+}
+
+func (c *CompressionType) UnmarshalJSON(data []byte) error {
+	switch string(data) {
+	case "zlib":
+		*c = CompressionZlib
+	default:
+		*c = CompressionNone
+	}
+	return nil
+}
+
+// CurrencyName
+
+type CurrencyName string
+
+// Asset
+
+// NOTE: there's also ExtendedAsset which is a quantity with the attached contract (AccountName)
+type Asset struct {
+	Amount int64
+	Symbol
+}
+
+func (a Asset) Add(other Asset) Asset {
+	if a.Symbol != other.Symbol {
+		panic("Add applies only to assets with the same symbol")
+	}
+	return Asset{Amount: a.Amount + other.Amount, Symbol: a.Symbol}
+}
+
+func (a Asset) Sub(other Asset) Asset {
+	if a.Symbol != other.Symbol {
+		panic("Sub applies only to assets with the same symbol")
+	}
+	return Asset{Amount: a.Amount - other.Amount, Symbol: a.Symbol}
+}
+
+func (a Asset) String() string {
+	strInt := fmt.Sprintf("%d", a.Amount)
+	if len(strInt) < int(a.Symbol.Precision+1) {
+		// prepend `0` for the difference:
+		strInt = strings.Repeat("0", int(a.Symbol.Precision+uint8(1))-len(strInt)) + strInt
+	}
+
+	var result string
+	if a.Symbol.Precision == 0 {
+		result = strInt
+	} else {
+		result = strInt[:len(strInt)-int(a.Symbol.Precision)] + "." + strInt[len(strInt)-int(a.Symbol.Precision):]
+	}
+
+	return fmt.Sprintf("%s %s", result, a.Symbol.Symbol)
+}
+
+// NOTE: there's also a new ExtendedSymbol (which includes the contract (as AccountName) on which it is)
+type Symbol struct {
+	Precision uint8
+	Symbol    string
+}
+
+// EOSSymbol represents the standard EOS symbol on the chain.  It's
+// here just to speed up things.
+var EOSSymbol = Symbol{Precision: 4, Symbol: "EOS"}
+
+func NewEOSAssetFromString(amount string) (out Asset, err error) {
+	val, err := strconv.ParseInt(strings.Replace(amount, ".", "", 1), 10, 64)
+	if err != nil {
+		return out, err
+	}
+	return NewEOSAsset(val), nil
+}
+
+func NewEOSAsset(amount int64) Asset {
+	return Asset{Amount: amount, Symbol: EOSSymbol}
+}
+
+// NewAsset parses a string like `1000.0000 EOS` into a properly setup Asset
+func NewAsset(in string) (out Asset, err error) {
+	sec := strings.SplitN(in, " ", 2)
+	if len(sec) != 2 {
+		return out, fmt.Errorf("invalid format %q, expected an amount and a currency symbol", in)
+	}
+
+	if len(sec[1]) > 7 {
+		return out, fmt.Errorf("currency symbol %q too long", sec[1])
+	}
+
+	out.Symbol.Symbol = sec[1]
+	amount := sec[0]
+	amountSec := strings.SplitN(amount, ".", 2)
+
+	if len(amountSec) == 2 {
+		out.Symbol.Precision = uint8(len(amountSec[1]))
+	}
+
+	val, err := strconv.ParseInt(strings.Replace(amount, ".", "", 1), 10, 64)
+	if err != nil {
+		return out, err
+	}
+
+	out.Amount = val
+
+	return
+}
+
+func (a *Asset) UnmarshalJSON(data []byte) error {
+	var s string
+	err := json.Unmarshal(data, &s)
+	if err != nil {
+		return err
+	}
+
+	asset, err := NewAsset(s)
+	if err != nil {
+		return err
+	}
+
+	*a = asset
+
+	return nil
+}
+
+type Permission struct {
+	PermName     string    `json:"perm_name"`
+	Parent       string    `json:"parent"`
+	RequiredAuth Authority `json:"required_auth"`
+}
+
+type PermissionLevel struct {
+	Actor      AccountName    `json:"actor"`
+	Permission PermissionName `json:"permission"`
+}
+
+// NewPermissionLevel parses strings like `account@active`,
+// `otheraccount@owner` and builds a PermissionLevel struct. It
+// validates that there is a single optional @ (where permission
+// defaults to 'active'), and validates length of account and
+// permission names.
+func NewPermissionLevel(in string) (out PermissionLevel, err error) {
+	parts := strings.Split(in, "@")
+	if len(parts) > 2 {
+		return out, fmt.Errorf("permission %q invalid, use account[@permission]", in)
+	}
+
+	if len(parts[0]) > 12 {
+		return out, fmt.Errorf("account name %q too long", parts[0])
+	}
+
+	out.Actor = AccountName(parts[0])
+	out.Permission = PermissionName("active")
+	if len(parts) == 2 {
+		if len(parts[1]) > 12 {
+			return out, fmt.Errorf("permission %q name too long", parts[1])
+		}
+
+		out.Permission = PermissionName(parts[1])
+	}
+
+	return
+}
+
+type PermissionLevelWeight struct {
+	Permission PermissionLevel `json:"permission"`
+	Weight     uint16          `json:"weight"` // weight_type
+}
+
+type Authority struct {
+	Threshold uint32                  `json:"threshold"`
+	Keys      []KeyWeight             `json:"keys,omitempty"`
+	Accounts  []PermissionLevelWeight `json:"accounts,omitempty"`
+	Waits     []WaitWeight            `json:"waits,omitempty"`
+}
+
+type KeyWeight struct {
+	PublicKey ecc.PublicKey `json:"key"`
+	Weight    uint16        `json:"weight"` // weight_type
+}
+
+type WaitWeight struct {
+	WaitSec uint32 `json:"wait_sec"`
+	Weight  uint16 `json:"weight"` // weight_type
+}
+
+type GetCodeResp struct {
+	AccountName AccountName `json:"account_name"`
+	CodeHash    string      `json:"code_hash"`
+	WASM        string      `json:"wasm"`
+	ABI         ABI         `json:"abi"`
+}
+
+type GetABIResp struct {
+	AccountName AccountName `json:"account_name"`
+	ABI         ABI         `json:"abi"`
+}
+
+// JSONTime
+
+type JSONTime struct {
+	time.Time
+}
+
+const JSONTimeFormat = "2006-01-02T15:04:05"
+
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%q", t.Format(JSONTimeFormat))), nil
+}
+
+func (t *JSONTime) UnmarshalJSON(data []byte) (err error) {
+	if string(data) == "null" {
+		return nil
+	}
+
+	t.Time, err = time.Parse(`"`+JSONTimeFormat+`"`, string(data))
+	return err
+}
+
+// HexBytes
+
+type HexBytes []byte
+
+func (t HexBytes) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hex.EncodeToString(t))
+}
+
+func (t *HexBytes) UnmarshalJSON(data []byte) (err error) {
+	var s string
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		return
+	}
+
+	*t, err = hex.DecodeString(s)
+	return
+}
+
+// SHA256Bytes
+
+type SHA256Bytes []byte // should always be 32 bytes
+
+func (t SHA256Bytes) MarshalJSON() ([]byte, error) {
+	return json.Marshal(hex.EncodeToString(t))
+}
+
+func (t *SHA256Bytes) UnmarshalJSON(data []byte) (err error) {
+	var s string
+	err = json.Unmarshal(data, &s)
+	if err != nil {
+		return
+	}
+
+	*t, err = hex.DecodeString(s)
+	return
+}
+
+type Varuint32 uint32
+
+// Tstamp
+
+type Tstamp struct {
+	time.Time
+}
+
+func (t Tstamp) MarshalJSON() ([]byte, error) {
+	return json.Marshal(fmt.Sprintf("%d", t.UnixNano()))
+}
+
+func (t *Tstamp) UnmarshalJSON(data []byte) (err error) {
+	var unixNano int64
+	if data[0] == '"' {
+		var s string
+		if err = json.Unmarshal(data, &s); err != nil {
+			return
+		}
+
+		unixNano, err = strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			return err
+		}
+
+	} else {
+		unixNano, err = strconv.ParseInt(string(data), 10, 64)
+		if err != nil {
+			return err
+		}
+	}
+
+	*t = Tstamp{time.Unix(0, unixNano)}
+
+	return nil
+}
+
+type BlockTimestamp struct {
+	time.Time
+}
+
+const BlockTimestampFormat = "2006-01-02T15:04:05"
+
+func (t BlockTimestamp) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%q", t.Format(BlockTimestampFormat))), nil
+}
+
+func (t *BlockTimestamp) UnmarshalJSON(data []byte) (err error) {
+	if string(data) == "null" {
+		return nil
+	}
+
+	t.Time, err = time.Parse(`"`+BlockTimestampFormat+`"`, string(data))
+	if err != nil {
+		t.Time, err = time.Parse(`"`+BlockTimestampFormat+`Z07:00"`, string(data))
+	}
+	return err
+}
+
+type JSONFloat64 float64
+
+func (f *JSONFloat64) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 {
+		return errors.New("empty value")
+	}
+
+	if data[0] == '"' {
+		var s string
+		if err := json.Unmarshal(data, &s); err != nil {
+			return err
+		}
+
+		val, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			return err
+		}
+
+		*f = JSONFloat64(val)
+
+		return nil
+	}
+
+	var fl float64
+	if err := json.Unmarshal(data, &fl); err != nil {
+		return err
+	}
+
+	*f = JSONFloat64(fl)
+
+	return nil
+}
+
+type JSONInt64 int64
+
+func (i *JSONInt64) UnmarshalJSON(data []byte) error {
+	if len(data) == 0 {
+		return errors.New("empty value")
+	}
+
+	if data[0] == '"' {
+		var s string
+		if err := json.Unmarshal(data, &s); err != nil {
+			return err
+		}
+
+		val, err := strconv.ParseInt(s, 10, 64)
+		if err != nil {
+			return err
+		}
+
+		*i = JSONInt64(val)
+
+		return nil
+	}
+
+	var v int64
+	if err := json.Unmarshal(data, &v); err != nil {
+		return err
+	}
+
+	*i = JSONInt64(v)
+
+	return nil
+}
